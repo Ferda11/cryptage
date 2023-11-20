6a76787fc7d34ffb86331e0f30fb180c4d823c89
Fleurimond Ferda Carine <ferdacarinef@gmail.com>
2023-11-20 14:30:15 -0500

    final

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='ferdacarinef@gmail.com',
     description='package pour encryter et decrytper des mots',
     url='https://github.com/Ferda11/cryptage',
-    version='1.0',
+    version='0.2',
     python_requires='>=3.10',
     packages=["cryptage"]    
 )
